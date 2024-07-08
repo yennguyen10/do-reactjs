2588328862a08e1c68b556646170409bbc5b7649
yennguyen10 <yen.nguyen@digitaloutlook.com.au>
2024-07-08 14:57:35 +0700

    updated

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import React from 'react';
 /*import Demo1 from './components/Demo1';
 import Demo2 from './components/Demo2';
 import Demo3 from './components/Demo3';
-import Demo from './components/Demo';
+
 import TodoFeatures from './features/Todo';
 import Clock from './components/Clock';*/
-import Bloglist from './components/Post';
+import Demo from './components/Demo';
 
 function App() {
   return (
     <div className="App">    
-      <Bloglist/>
+      
 
-      {/*<Demo/>  
-      <Demo1 />
+      <Demo/>  
+      {/*<Demo1 />
       <Demo2 />
       <Demo3 />
       <TodoFeatures/>
